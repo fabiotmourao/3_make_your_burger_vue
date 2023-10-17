4bf67b08add38a818d12b352e7f0b24a6f79ba8a
Fábio Teixeira <fabio.teixeira@ipremi.com.br>
2023-10-17 20:33:24 -0300

    iniciando projeto/estilizando areas do corpo/site

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="footer">
+        <p>Make your Burger &copy; 2023 </p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+    .footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="main-container">
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
